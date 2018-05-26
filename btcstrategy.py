85f0328089f198b3729fc8136a0cba5880b7b2e3
wangzixuan <wangzixuan@youzan.com>
2018-05-26 20:26:55 +0800

    init

---
+++ btcstrategy.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from HuobiService import *
+
+# logging.basicConfig()
+
+open = close = high = low = cur = 0
+
+# 建仓点位
+openPosition = 0
+# 规则触发次数
+actionNum = 0
+# 规则成功次数
+actionSuc = 0
+
+# 成功累计盈利
+totalProfit = 0
+
+minus = -1
+action = 'buy'
+
+# 是否建仓
+order = False
+
+# 波动率
+checkPointRate = 0.002
+# 几分钟K线
+KType = '5min'
+# 几分钟K线
+MMN = 5
+# 平仓时间
+nextMinTime = 0
+# 交易对
+pair = 'btcusdt'
+# 下单量
+value = '0.001'
+import sys
+
+output = sys.stdout
+
+
+def sellAction(price):
+    print '\n --------------------sellAction:' + price
+    send_order(value, '', pair, 'sell-limit', price)
+
+
+def buyAciton(price):
+    print '\n --------------------buyAction:' + price
+    send_order(value, '', pair, 'buy-limit', price)
+
+
+def waitCheckPoint():
+    global open
+    global cur
+    global high
+    global low
+    global close
+
+    global minus
+    global action
+    global order
+    global nextMinTime
+
+    kline = get_kline(pair, KType, '1')
+    # print(kline)
+
+    open = kline['data'][0]['open']
+    high = kline['data'][0]['high']
+    low = kline['data'][0]['low']
+    cur = kline['data'][0]['close']
+
+    checkpoint = open * checkPointRate
+
+    if high - open > open - low:
+        action = 'sell'
+        minus = cur - low
+    elif high - open <= open - low:
+        action = 'buy'
+        minus = high - cur
+
+    now = datetime.datetime.now()
+
+    # print(" time:" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+    sucRate = 0
+    if actionSuc != 0:
+        sucRate = actionSuc / float(actionNum)
+
+    # output.write('\r cur:' + str(cur)
+    #              + " minus:" + str(minus)
+    #              + " CP:" + str(checkpoint)
+    #              + " ACT:" + str(action)
+    #              + " O:" + str(open)
+    #              + " H:" + str(high)
+    #              + " L:" + str(low)
+    #              + " ACTNUM:" + str(actionNum)
+    #              + " sRate:" + str(sucRate)
+    #              + " tProfit:" + str(totalProfit)
+    #              + " time:" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+    # sys.stdout.flush()
+    if minus >= checkpoint:
+        order = True
+        job2()
+        nextMinTime = MMN * (now.minute / MMN + 2) % 60
+        output.write('\r ++++++++++++   get checkpoint: cur:' + str(cur)
+                     + " minus:" + str(minus)
+                     + " CP:" + str(checkpoint)
+                     + " ACT:" + str(action)
+                     + " O:" + str(open)
+                     + " H:" + str(high)
+                     + " L:" + str(low)
+                     + " ACTNUM:" + str(actionNum)
+                     + " sRate:" + str(sucRate)
+                     + " tProfit:" + str(totalProfit)
+                     + " time:" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+                     + " nextMinTime:" + str(nextMinTime))
+        sys.stdout.flush()
+        # print ('\n ##############   get checkpoint' + 'minus:' + str(minus)
+        #        + " checkpoint:" + str(checkpoint)
+        #        + " action:" + str(action)
+        #        + " open:" + str(open)
+        #        + " high:" + str(high)
+        #        + " low:" + str(low)
+        #        + " actionNum:" + str(actionNum)
+        #        + " time:" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+        #        + " nextMinTime:" + str(nextMinTime))
+
+
+def job():
+    global close
+    global order
+    global actionSuc
+    global totalProfit
+
+    now = datetime.datetime.now()
+    # print now.year, now.month, now.day, now.hour, now.minute, now.second
+
+    if order:
+        # output.write(
+        #     '\r openPosition：' + str(openPosition) + " response... wait for:" + str(nextMinTime) + " " + str(
+        #         now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+        # sys.stdout.flush()
+        # print "openPosition：", openPosition, " response... wait for:", nextMinTime, now.year, now.month, now.day, now.hour, now.minute, now.second
+        if now.second <= 10 and now.minute == nextMinTime:
+            print "\n ----------  time to reaction"
+            kline = get_kline(pair, KType, '1')
+            close = kline['data'][0]['close']
+            if action == 'buy':
+                if cur < close:
+                    actionSuc += 1
+                    totalProfit += (close - openPosition) * float(value) - 0.002 * close * float(
+                        value) - 0.002 * openPosition * float(value)
+                    print "\n 平仓卖", " openPosition：", openPosition, " closePosition:", close
+                    sellAction(str(close))
+                else:
+                    print '\n 低买高卖失败', " openPosition：", openPosition, " closePosition:", close
+            elif action == 'sell':
+                if cur > close:
+                    actionSuc += 1
+                    totalProfit += (openPosition - close) * float(value) - 0.002 * close * float(
+                        value) - 0.002 * openPosition * float(value)
+                    print "\n 平仓买", " openPosition：", openPosition, " closePosition:", close
+                    buyAciton(str(close))
+                else:
+                    print '\n 高卖低买失败', " openPosition：", openPosition, " closePosition:", close
+
+            order = False
+        return
+    else:
+        waitCheckPoint()
+
+
+def job2():
+    global actionNum
+    global openPosition
+    openPosition = cur
+    actionNum += 1
+    if action == 'buy':
+        buyAciton(str(cur))
+    elif action == 'sell':
+        sellAction(str(cur))
+
+
+print('\n start')
+# 定义BlockingScheduler
+sched = BlockingScheduler()
+# sched = BackgroundScheduler()
+sched.add_job(job, 'interval', seconds=1)
+# sched.add_job(job2, 'interval', seconds=2)
+sched.start()
---
+++ sss.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# def test():
+#     time = 30
+#     MMN = 15
+#     N = time / MMN
+#
+#     print N
+#     nexttime = MMN * (N + 2) % 60
+#     print(nexttime)
+#
+#
+# test()
+
+
+import datetime
+import sys
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# def viewBar(i):
+#     output = sys.stdout
+#     for count in range(0, i + 1):
+#         now = datetime.datetime.now()
+#         second = 0.1
+#         sleep(second)
+#         # output.write('\r running...' + str(now.second))
+#         print('\r start')
+#     # output.flush()
+#     print(1 / float(3))
+#
+#
+# viewBar(100)
+# for i in range(5):
+#     sys.stdout.write(' ' * 10 + '\r')
+#     sys.stdout.flush()
+#     sys.stdout.write(str(i) * (5 - i) + '\r')
+#     sys.stdout.flush()
+#     time.sleep(1)
+# from datetime import datetime
+
+output = sys.stdout
+
+
+def job():
+    now = datetime.datetime.now()
+
+    # print "\r Hello, Gay! ", now.second,
+    # sys.stdout.flush()
+    output.write("\r time:" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+    sys.stdout.flush()
+
+
+# 定义BlockingScheduler
+sched = BlockingScheduler()
+# sched = BackgroundScheduler()
+sched.add_job(job, 'interval', seconds=1)
+# sched.add_job(job2, 'interval', seconds=2)
+sched.start()
+
+# now = datetime.datetime.now()
+# print("ssss")
+# print(" time:" + str(now.day) + " " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+
+# import curses
+# pad = curses.newpad(100, 100)
+# #  These loops fill the pad with letters; this is
+# # explained in the next section
+# for y in range(0, 100):
+#     for x in range(0, 100):
+#         try: pad.addch(y,x, ord('a') + (x*x+y*y) % 26 )
+#         except curses.error: pass
+#
+# #  Displays a section of the pad in the middle of the screen
+# pad.refresh( 0,0, 5,5, 20,75)
+#
